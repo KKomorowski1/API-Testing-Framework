e87ee21b0177f8dfd65c98cb4159a584d1ce98ff
kacper <kakacper23@gmail.com>
2026-02-13 16:36:32 +0100

    Revert "added simple test and added locust library for perf testing"
    This reverts commit ebf913fa9192c17233d375976ff3b40ca86d0b85.

--- tests/test_booking_ddt.py
+++ tests/test_booking_ddt.py
@@ -1,11 +1,7 @@
 import pytest
 from config.config import config  # Import the instance
-from utils.data_processor import DataProcessor
 from services.auth_service import AuthService
 from services.booking_service import BookingService
-from schemas.booking_schema import BookingResponse
-
-valid_bookings = DataProcessor.load_and_process_json("booking_valid.json")
 
 @pytest.fixture(scope="session")
 def auth_token():
@@ -17,15 +13,3 @@ def booking_service(auth_token):
     service = BookingService(config.BASE_URL)
     service.set_auth_cookie(auth_token)
     return service
-
-
-@pytest.mark.parametrize("booking_data", valid_bookings)
-def test_create_booking_success(booking_service, booking_data):
-    # 1. Act: Call the service to create a booking
-    response = booking_service.create_booking(booking_data)
-    
-    # 2. Assert: Status code should be 200
-    assert response.status_code == 200
-    
-    # 3. Assert: Validate response structure using Pydantic schema
-    BookingResponse(**response.json())
